bfa38beefcd050e4fd96a2129976dbdada69e110
arxs-abd <70242781+arxs-abd@users.noreply.github.com>
2022-10-23 18:50:24 +0800

    update note module

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,39 @@
-const mongoose = require('mongoose')
+const express = require('express')
+const { Note } = require('../model/note')
+const router = express.Router()
+
+router.get('/api/note', async (req, res) => {
+    return res.send({
+        status : 'success',
+        data : {
+            note : 'Ini Notenya'
+        }
+    })
+})
+
+router.post('/api/note', async (req, res) => {
+    const { title, decsription } = req.body
+    const data = {title, decsription}
+    const newNote = new Note(data)
+    await newNote.save()
+
+    return res.send({
+        status : 'success',
+        msg : 'Note add successfully'
+    })
+})
+
+router.delete('/api/note', async (req, res) => {
+    const { _id } = req.body
+    const note = await Note.findOne({
+        _id
+    })
+    if (!note) return res.send({
+        status : 'error',
+        msg : 'Cannot find the note'
+    })
+    return res.send({
+        status : 'success',
+        msg : 'Note delete successfully'
+    })
+})
