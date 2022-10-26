22c98c496ab44c1323f4becdf1d3a949949f1c9d
arxs-abd <70242781+arxs-abd@users.noreply.github.com>
2022-10-26 20:59:41 +0800

    update update note

--- routes/notes.js
+++ routes/notes.js
@@ -13,15 +13,6 @@ router.get('/api/note', async (req, res) => {
     })
 })
 
-// router.get('/api/note/:id', async (req, res) => {
-//     return res.send({
-//         status : 'success',
-//         data : {
-//             note : 'Ini Notenya'
-//         }
-//     })
-// })
-
 router.post('/api/note', async (req, res) => {
     const { title, description } = req.body
     const token = jwt.verify(req.cookies['x-access-token'], 'SECRET')
@@ -40,8 +31,32 @@ router.post('/api/note', async (req, res) => {
     })
 })
 
+router.put('/api/note', async (req, res) => {
+    const { id, title, description } = req.body
+    const token = jwt.verify(req.cookies['x-access-token'], 'SECRET')
+    const user = await User.findOne({
+        _id : token.user._id
+    })
+    const allNote = user.note
+    const newNote = allNote.map(note => {
+        if (String(note._id) === id) {
+            note.title = title
+            note.description = description
+            return note
+        } else {
+            return note
+        }
+    })
+    user.note = newNote
+    await user.save()
+    return res.send({
+        status : 'success',
+        msg : 'Note update successfully'
+    })
+})
+
 router.delete('/api/note', async (req, res) => {
-    const { _id } = req.body
+    const { id } = req.body
     const token = jwt.verify(req.cookies['x-access-token'], 'SECRET')
     const user = await User.findOne({
         _id : token.user._id
@@ -49,7 +64,7 @@ router.delete('/api/note', async (req, res) => {
     const allNote = user.note
     const newNotes = allNote.filter(note => {
         // console.log(String(note._id) !== _id)
-        return String(note._id) !== _id
+        return String(note._id) !== id
     })
     user.note = newNotes
     await user.save()
