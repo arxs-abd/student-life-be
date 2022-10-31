b7c48ed9369450c335a44f9bf22721e3cc6c68ee
arxs-abd <70242781+arxs-abd@users.noreply.github.com>
2022-10-31 17:32:09 +0800

    update bug localhost

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const routeAuth = require('./routes/auth')
 const routeNote = require('./routes/notes')
 const port = process.env.PORT || 4000
 
+app.use(express.urlencoded())
 app.use(cors())
 app.use(cookieParser())
 app.use(bodyParser.json())
